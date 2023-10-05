8b6e2fd7c43da93d0dcb56c57ae6cb1ceb9b0dad
Matias Pereiro <matienzof6@gmail.com>
2023-10-05 18:59:30 +0200

    Creando y asignando las paginas

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import store from "store";
 import { Provider } from "react-redux";
 import {NextUIProvider} from "@nextui-org/react";
+import About from "containers/pages/About";
+import Contact from "containers/pages/Contact";
+import Services from "containers/pages/Services";
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
           <Routes>
             <Route path="*" element={<Error404 />} />
             <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/services" element={<Services />} />
           </Routes>
         </BrowserRouter>
       </Provider>
---
+++ src/containers/pages/Services.jsx
@@ -0,0 +1,17 @@
+
+import Footer from "components/navigation/Footer"
+import Navbar from "components/navigation/Navbar"
+import Layout from "hocs/layouts/Layout"
+
+function Services(){
+    return(
+        <Layout>
+            <Navbar/>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore nisi impedit possimus blanditiis dolores deleniti vitae, adipisci, minima ea iusto pariatur! Aspernatur saepe tenetur rerum et corporis. A, sed cum!
+            <Footer/>
+        </Layout>
+    )
+}
+
+
+export default Services
